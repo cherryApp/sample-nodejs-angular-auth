fadad0a40dfdf868460b4457094fd28545bb4d2a
Csernak Tamas <csernak.tamas91@gmail.com>
2018-05-11 09:19:42 +0200

    Feat: added sticky variable.

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   copyRight = '&copy; 2018 cherryApp';
+  sticky : boolean = true;
   constructor() { }
 
   ngOnInit() {
