5b71e3fa0ff9801b7b4e37cdcdb3ee6e843d10d0
cherryApp <j.cserko@gmail.com>
2018-05-11 09:51:50 +0200

    Revert "Feat: change sticky property."
    This reverts commit 715cf2cb1f11405ecb67502922381eff68455399.

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -8,9 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
   copyRight = '&copy; 2018 cherryApp';
   sticky : boolean = true;
-  constructor() {
-    this.sticky = false;
-  }
+  constructor() { }
 
   ngOnInit() {
   }
