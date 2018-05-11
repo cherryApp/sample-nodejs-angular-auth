f7a400550e8967586ccef51b2d72c1a9fdfe49c9
cherryApp <j.cserko@gmail.com>
2018-05-11 09:14:40 +0200

    Feat: add copy text to the footer component

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  copyRight = '&copy; 2018 cherryApp';
   constructor() { }
 
   ngOnInit() {
